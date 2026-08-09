48a7a154f75fb0a107b9d6627033b1594d513c7b
Lucas <lucasdssilva13@hotmail.com>
2026-08-09 16:13:55 -0300

    Integração com sql e backend online

--- backend/server.js
+++ backend/server.js
@@ -19,23 +19,19 @@ app.use(express.json());
 // ======================================================
 
 const db = mysql.createPool({
-
     host: process.env.DB_HOST,
-
     user: process.env.DB_USER,
-
     password: process.env.DB_PASSWORD,
-
     database: process.env.DB_NAME,
-
     port: Number(process.env.DB_PORT) || 3306,
 
+    ssl: {
+        rejectUnauthorized: false
+    },
+
     waitForConnections: true,
-
     connectionLimit: 10,
-
     queueLimit: 0
-
 });
 
 
